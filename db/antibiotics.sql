120d526f7c425ff36d9af4b96719d288c0de4f35
araxis07 <pavornprat.aa@gmail.com>
2024-10-16 18:55:41 +0700

    change all

--- db/antibiotics.sql
+++ db/antibiotics.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2024 at 10:06 AM
+-- Generation Time: Oct 16, 2024 at 01:52 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -37,22 +37,23 @@ CREATE TABLE `account` (
   `images_account` varchar(50) DEFAULT NULL,
   `login_count_account` int(1) NOT NULL,
   `lock_account` int(1) NOT NULL,
-  `ban_account` datetime DEFAULT NULL
+  `ban_account` datetime DEFAULT NULL,
+  `reported_count` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`id_account`, `username_account`, `email_account`, `password_account`, `salt_account`, `role_account`, `images_account`, `login_count_account`, `lock_account`, `ban_account`) VALUES
-(2, 'member', 'member@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$aWZiNVNaSi8vVG5OcGlmNg$nBoPY0lPdB9GC/nzo2LIStP5b4VV/Iw9bWrFcFLiE0M', 'ab21409ae4d3d48840635b02c27be7af069b9a9a4c7798c1d2b1e654f6fdb15fec2691b745f90315879eaada67ad4148a49fb49717c090feafcaaada55b9070de1f89c9530632d4167ae8c970eb1d32efbf86cf33bcc641c233a0fa1790261c36a9fd9a9e70186959ca5e33557d9af5156940fbe13', 'member', 'default_images_account.jpg', 1, 0, '0000-00-00 00:00:00'),
-(3, 'Admin', 'admin@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$bXMxekU2U1l5LlJSakQ5Qw$auQgIBXB+ht4bhCCdC5/MsSEOTWvCNypVapTPQz/jKk', '4d02ab0b6d9642c678a899183403be36abb8f6f00e95f1b11e4315cbfb80e7ec9b53151291a303f968df1e6545518b06cc2b749ff4af38cf39603e2f6a0b5a85cf6f8e3fd236610c6839ba9c260f3bf3e277406d9fdf35c7d28d18b7fad54383e302cf94f9f4f9709e93b40fa8d23cef', 'admin', 'default_images_account.jpg', 0, 0, '2024-10-16 14:51:38'),
-(4, 'San', 'Santas1s@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SzZPOTVlNjI1emxCNHM5Yw$heD/Bnf1NebRTXGDoWilvbGeQQuqYIZQu8nhA+hWi6s', 'be64419531ee7c4495a3c426d551adc30923cdfbad1c29b674704db21efadb6e584e733658cef335c3f5948b2420aa07fc459933dd13f847458ac968041b78d2fcec75fd54c2e4250c09305066b6b45ba55e5661d2f70302c3b88ff5c159f57da0', 'member', 'default_images_account.jpg', 1, 0, '0000-00-00 00:00:00'),
-(5, 'gg', 'gg@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UzNYMlRsYTNZZ1ZJVXk5TQ$3bo7zx2qP8DhRnDdaMkrb5GxzTuWKL0spE6X1fNFlmA', '51d847ceebc686c714548d07dc9e71272e8926f122575638c08121746c65e6b366502caf3a82c49fb4c4b71afa4da7e08ddd9cb1265c95e48d8239822f425984d3da536b48c1375430134715f1f7f6f8fc8cb6d8fb9a007d72366b1503b2f327a8d4410dd0ddd168fa24b8393515e08d778a', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00'),
-(6, 'ggez', 'ggez@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UnZmcDFiZHJRYVgxc01Pcg$i9wbnPRa1hDHaF6cjKIdcIEuNfLEelBsW39GXooU5ZU', '5ee5c29620b0588e1209c61be91300ae78d74032cdb8bcb2ade82e434292cf8f26de259854bcd59106d21c1dcd008be7254664cb62e9dc3157187b0792f3822248f1d271b08bb32212082846445c967fdfd5bff7f3faf66bc5d2b9b9d2d50b58fbae293f087a928f', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00'),
-(7, 'ez', 'ez@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$S2hJM2pVUk1rQkNCMVVKdg$W89xqZXhJcJlz0ZhoOO/z/R8zalpEY6P96+SY0/mrhU', '07f75482c88e66febe8e62394fc295d81d3d2c9e215a818f36dcf97ee56f8a284fe32c8ddb1deae1cefc538157bb4c30f70e9a377f0ec2462dd31ae578bb0ec0ccf5d0cf06334046498a6b8497d83063b6c23fca41ab5c09b9d21420bac1309137aaea88511767c3a4d1ee7215', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00'),
-(8, 'bite', 'bite@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$cC5aWDUwalkuaVdtTm5Qbw$rfsq8t89iEma3ktXFJJ/qQTIsWC2Oj78Y4AXy498Sbw', '167259d797c172147a8cf1f96c1e9d1dc0bcf0dc6e7ec326764370b5b2bbca5b4c2c9ab2605b78c50cabe2aaed20bf7d857c6c7315c264b5de94fee63aee504a2984d92bc20da813a753289c2c37482a7da5e5fb1f544b4d1b0c01ac86c131e82fb8f643e0e80d', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00'),
-(9, 'aon', 'aon@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SFM1SnZrVklpb3c1WE9CdQ$U6KktiScJ+ZK66yetIRXUdhuIhiqQ9cgzOcDsFRnYrI', '004514f6900496271e896e0e601dadef0c0899df4cad90190254921a24a0b179f31f0576e3394c98082c1cd60a42bc1f9b98e7b317c6351557f58cdbfeb04cbacb103ee3c78d93678fafe417f3cbb13fd2bd0263efd3ed9027844e89833305fe31fe04', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00');
+INSERT INTO `account` (`id_account`, `username_account`, `email_account`, `password_account`, `salt_account`, `role_account`, `images_account`, `login_count_account`, `lock_account`, `ban_account`, `reported_count`) VALUES
+(2, 'member', 'member@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$aWZiNVNaSi8vVG5OcGlmNg$nBoPY0lPdB9GC/nzo2LIStP5b4VV/Iw9bWrFcFLiE0M', 'ab21409ae4d3d48840635b02c27be7af069b9a9a4c7798c1d2b1e654f6fdb15fec2691b745f90315879eaada67ad4148a49fb49717c090feafcaaada55b9070de1f89c9530632d4167ae8c970eb1d32efbf86cf33bcc641c233a0fa1790261c36a9fd9a9e70186959ca5e33557d9af5156940fbe13', 'member', 'default_images_account.jpg', 1, 0, '0000-00-00 00:00:00', 0),
+(3, 'Admin', 'admin@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$bXMxekU2U1l5LlJSakQ5Qw$auQgIBXB+ht4bhCCdC5/MsSEOTWvCNypVapTPQz/jKk', '4d02ab0b6d9642c678a899183403be36abb8f6f00e95f1b11e4315cbfb80e7ec9b53151291a303f968df1e6545518b06cc2b749ff4af38cf39603e2f6a0b5a85cf6f8e3fd236610c6839ba9c260f3bf3e277406d9fdf35c7d28d18b7fad54383e302cf94f9f4f9709e93b40fa8d23cef', 'admin', 'default_images_account.jpg', 0, 0, '2024-10-16 14:51:38', 0),
+(4, 'San', 'Santas1s@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SzZPOTVlNjI1emxCNHM5Yw$heD/Bnf1NebRTXGDoWilvbGeQQuqYIZQu8nhA+hWi6s', 'be64419531ee7c4495a3c426d551adc30923cdfbad1c29b674704db21efadb6e584e733658cef335c3f5948b2420aa07fc459933dd13f847458ac968041b78d2fcec75fd54c2e4250c09305066b6b45ba55e5661d2f70302c3b88ff5c159f57da0', 'member', 'default_images_account.jpg', 1, 0, '0000-00-00 00:00:00', 0),
+(5, 'gg', 'gg@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UzNYMlRsYTNZZ1ZJVXk5TQ$3bo7zx2qP8DhRnDdaMkrb5GxzTuWKL0spE6X1fNFlmA', '51d847ceebc686c714548d07dc9e71272e8926f122575638c08121746c65e6b366502caf3a82c49fb4c4b71afa4da7e08ddd9cb1265c95e48d8239822f425984d3da536b48c1375430134715f1f7f6f8fc8cb6d8fb9a007d72366b1503b2f327a8d4410dd0ddd168fa24b8393515e08d778a', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00', 0),
+(6, 'ggez', 'ggez@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$UnZmcDFiZHJRYVgxc01Pcg$i9wbnPRa1hDHaF6cjKIdcIEuNfLEelBsW39GXooU5ZU', '5ee5c29620b0588e1209c61be91300ae78d74032cdb8bcb2ade82e434292cf8f26de259854bcd59106d21c1dcd008be7254664cb62e9dc3157187b0792f3822248f1d271b08bb32212082846445c967fdfd5bff7f3faf66bc5d2b9b9d2d50b58fbae293f087a928f', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00', 1),
+(7, 'ez', 'ez@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$S2hJM2pVUk1rQkNCMVVKdg$W89xqZXhJcJlz0ZhoOO/z/R8zalpEY6P96+SY0/mrhU', '07f75482c88e66febe8e62394fc295d81d3d2c9e215a818f36dcf97ee56f8a284fe32c8ddb1deae1cefc538157bb4c30f70e9a377f0ec2462dd31ae578bb0ec0ccf5d0cf06334046498a6b8497d83063b6c23fca41ab5c09b9d21420bac1309137aaea88511767c3a4d1ee7215', 'member', 'default_images_account.jpg', 1, 0, '0000-00-00 00:00:00', 0),
+(8, 'bite', 'bite@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$cC5aWDUwalkuaVdtTm5Qbw$rfsq8t89iEma3ktXFJJ/qQTIsWC2Oj78Y4AXy498Sbw', '167259d797c172147a8cf1f96c1e9d1dc0bcf0dc6e7ec326764370b5b2bbca5b4c2c9ab2605b78c50cabe2aaed20bf7d857c6c7315c264b5de94fee63aee504a2984d92bc20da813a753289c2c37482a7da5e5fb1f544b4d1b0c01ac86c131e82fb8f643e0e80d', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00', 0),
+(9, 'aon', 'aon@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$SFM1SnZrVklpb3c1WE9CdQ$U6KktiScJ+ZK66yetIRXUdhuIhiqQ9cgzOcDsFRnYrI', '004514f6900496271e896e0e601dadef0c0899df4cad90190254921a24a0b179f31f0576e3394c98082c1cd60a42bc1f9b98e7b317c6351557f58cdbfeb04cbacb103ee3c78d93678fafe417f3cbb13fd2bd0263efd3ed9027844e89833305fe31fe04', 'member', 'default_images_account.jpg', 0, 0, '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -77,10 +78,6 @@ CREATE TABLE `d_patience` (
 --
 
 INSERT INTO `d_patience` (`username`, `total_score`, `comments`, `email`, `treat_score`, `treat_comments`, `docname`, `doccon`, `p_show_count`) VALUES
-('aon', 'ตวามเสี่ยงสูง', 'dasda', 'aon@gmail.com', 'สวดอภิธรรม', 'Message', 'โอม', 'ดาวอังคาร', 1),
-('bite', 'ตวามเสี่ยงสูง', 'asdasd', 'bite@gmail.com', 'รักษาใจก่อน', 'Messageasd', 'โอม', 'ดาวอังคาร', 1),
-('ez', 'ตวามเสี่ยงสูง', '', 'ez@gmail.com', NULL, NULL, NULL, NULL, 0),
-('gg', 'ตวามเสี่ยงสูง', 'sdasdsd', 'gg@gmail.com', 'รักษาใจก่อน', 'Message', 'โอม', 'ดาวอังคาร', 0),
 ('ggez', 'ตวามเสี่ยงสูง', 'sdads', 'ggez@gmail.com', 'ทำบุญเยอะๆ', 'Message', 'โอม', 'ดาวอังคาร', 1),
 ('member', 'ความเสี่ยงต่ำ', 'sadad', 'member@gmail.com', 'สวดอภิธรรม', '1d1d', 'โอม', 'ดาวอังคาร', 0),
 ('San', 'ตวามเสี่ยงสูง', 'DFQ', 'Santas1s@gmail.com', 'มาพบแพทย์ที่รพ.', 'Message', 'โอม', 'ดาวอังคาร', 0);
